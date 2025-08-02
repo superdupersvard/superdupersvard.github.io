42b430f03dbd36d5d076c8f294f4abbcfdf2c155
Johan Svärd <superdupersvard@users.noreply.github.com>
2025-08-02 17:30:57 +0200

    Update server_settings.js

--- server_settings.js
+++ server_settings.js
@@ -1,8 +1,8 @@
 const backendBaseUrl = 'https://api.mtbokartan.se';
 // const backendBaseUrl = '';
 
-//const tileUrl = 'https://storage.googleapis.com/mtbo-map-tiles/Tiles/{z}/{x}/{-y}.webp';
-const tileUrl = './tiles/{z}/{x}/{-y}.webp';
+const tileUrl = 'https://storage.googleapis.com/mtbo-map-tiles/Tiles/{z}/{x}/{-y}.webp';
+//const tileUrl = './tiles/{z}/{x}/{-y}.webp';
 
 export function getBackendBaseUrl() {
     return backendBaseUrl;
